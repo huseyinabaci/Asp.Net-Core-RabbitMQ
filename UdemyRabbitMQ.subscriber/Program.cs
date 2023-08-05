49dc599263b25080a0c7c957b4951490a5059032
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2023-08-05 19:11:54 +0300

    Update

--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -13,8 +13,13 @@ var channel = connection.CreateModel();
 channel.BasicQos(0, 1, false);
 var consumer = new EventingBasicConsumer(channel);
 
-var queueName = "direct-queue-Critical";
+var queueName = channel.QueueDeclare().QueueName;
 
+var routeKey = "*.Error.*";
+
+channel.QueueBind(queueName, "logs-topic", routeKey);
+
+channel.BasicConsume(queueName, false, consumer);
 
 Console.WriteLine("Logları dinleniyor...");
 
